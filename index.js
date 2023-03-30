d8d672bde5bbff6fa3c93b191926ca6d8af4a128
Dwayna Harris <dwaynaharris@gmail.com>
2023-03-30 19:03:40 -0400

    practice using builtin objects and arithmetic to solve javascript code challenges

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 2; 
+let num2 = 31;
+const multiply = num1*num2;
+
+let random = Math.random();
+if (random === 0) {
+    random += 1;
+} else if (random > 0) {
+    random = Math.ceil(random);
+}
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(2, 8, 10, 20);
